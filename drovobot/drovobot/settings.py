9ba1a72ce402390684e657cff851235d674c51fb
kaizerj <kzrster@gmail.com>
2019-12-11 00:00:39 +0900

    fix

--- drovobot/drovobot/settings.py
+++ drovobot/drovobot/settings.py
@@ -143,7 +143,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'en-en'
 
 TIME_ZONE = 'Asia/Irkutsk'
 
