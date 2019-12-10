34d6452a65c0f69f7a895b8d070545e6826f7e01
kaizerj <kzrster@gmail.com>
2019-12-11 00:24:59 +0900

    add viber app to django

---
+++ drovobot/viber_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ViberConfig(AppConfig):
+    name = 'viber_app'
---
+++ drovobot/viber_app/views.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+
+from viberbot import Api
+from viberbot.api.bot_configuration import BotConfiguration
+from viberbot.api.messages import VideoMessage
+from viberbot.api.messages import (
+        TextMessage,
+        ContactMessage,
+        PictureMessage,
+        VideoMessage
+    )
+from viberbot.api.messages.data_types.contact import Contact
+
+from viberbot.api.viber_requests import ViberConversationStartedRequest
+from viberbot.api.viber_requests import ViberFailedRequest
+from viberbot.api.viber_requests import ViberMessageRequest
+from viberbot.api.viber_requests import ViberSubscribedRequest
+from viberbot.api.viber_requests import ViberUnsubscribedRequest
+
+from django.http import HttpResponse
+from django.views import View
+
+
+class ViberView(View):
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Hello, World!')
+
+    def post(self, request, *args, **kwargs):
+        print(request)
+        print(request.POST)
+        print('Oppa')
+        return HttpResponse('Hello, World!')
