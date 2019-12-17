067afab094172e7a133e103889ad394e2765b73c
kaizerj <kzrster@gmail.com>
2019-12-17 22:19:44 +0900

    add admin

--- drovobot/main/admin.py
+++ drovobot/main/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+
+from main.models import Customer, Ad
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Customer._meta.fields]
+
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Ad._meta.fields]
