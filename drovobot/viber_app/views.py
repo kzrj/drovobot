40714778ecbbb01aae47562dc89ff6e3dc82541e
kaizerj <kzrster@gmail.com>
2019-12-17 22:48:27 +0900

    write phone

--- drovobot/viber_app/views.py
+++ drovobot/viber_app/views.py
@@ -142,7 +142,7 @@ def viber_view(request):
     elif isinstance(viber_request, ViberConversationStartedRequest):
         # viber_send_main_menu(viber, viber_request.user.id)
         viber.send_messages(viber_request.user.id, [
-            RichMediaMessage(rich_media=SAMPLE_RICH_MEDIA, alt_text='ALT TEXT!', min_api_version=2)
+            RichMediaMessage(rich_media=SAMPLE_RICH_MEDIA, alt_text='ALT TEXT!', min_api_version=3)
         ])
 
     elif isinstance(viber_request, ViberSubscribedRequest):
