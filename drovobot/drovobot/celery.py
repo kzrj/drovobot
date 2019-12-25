05a63759b6e071c394990442ccdf1c73c447599e
kaizerj <kzrster@gmail.com>
2019-12-25 23:41:38 +0900

    celery

--- drovobot/drovobot/celery.py
+++ drovobot/drovobot/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drovobot.settings')
-app = Celery('ecommerce', broker='amqp://admin:qwerty123@rabbit:5672', backend='rpc://')
+app = Celery('drovobot', broker='amqp://admin:qwerty123@rabbit:5672', backend='rpc://')
     
 app.conf.broker_heartbeat = 0
 
