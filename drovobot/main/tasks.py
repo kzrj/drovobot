43ff7d8d04cd0e7bbe4fadbf5ace900bdc0941d0
kaizerj <kzrster@gmail.com>
2020-01-08 22:58:56 +0900

    test celery

--- drovobot/main/tasks.py
+++ drovobot/main/tasks.py
@@ -3,6 +3,8 @@ from celery import shared_task
 
 
 @shared_task
-def celery_deactivate_ad(subject, ctx, sender, receiver, template):
+def deactivate_ad():
     print("OOOOOOOOOPAPAPPAPAAAAAAAAAAAAAAAAAAAAAAAAPAPAP@!!!!!!!!!!!!!!")
+    # ad.deactivate
+    print("DEACTIVATE")
 
