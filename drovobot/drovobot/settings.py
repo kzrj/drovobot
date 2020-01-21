112e59f20135a2f2221325b467de535642686b13
kaizerj <kzrster@gmail.com>
2020-01-22 00:37:14 +0900

    test static

--- drovobot/drovobot/settings.py
+++ drovobot/drovobot/settings.py
@@ -155,7 +155,7 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/backstatic/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
